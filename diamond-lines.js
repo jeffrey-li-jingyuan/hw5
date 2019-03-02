07f5e760a3f254baa9514d2fb2b070495a16687d
jeffrey-li-jingyuan <47227251+jeffrey-li-jingyuan@users.noreply.github.com>
2019-03-01 18:30:42 -0800

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -1,1 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
 
+function draw() {
+  background(220);
+  for (var i = 10; i <= 200; i = i + 10) {
+    line(210 - i, i, i + 190, i);
+  }
+  for (var m = 200; m <= 390; m = m + 10) {
+    line(m - 190, m, 590 - m, m);
+  }
+}
